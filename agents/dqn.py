1a1d7cbfc1a7fd261b757f540988a379ad78ef5f
BluxWarrior <warriorblux@gmail.com>
2023-10-09 12:32:27 +0800

    first commit

---
+++ agents/dqn.py
@@ -0,0 +1,66 @@
+from __future__ import division
+import warnings
+
+import keras.backend as K
+import numpy as np
+from keras.models import Model
+from keras.layers import Lambda, Input, Layer, Dense
+
+from upper.core import Agent
+from upper.policy import EpsGreedyQPolicy, GreedyQPolicy
+from upper.utils import *
+
+
+def mean_q(y_true, y_pred):
+    return K.mean(K.max(y_pred, axis=-1))
+
+
+class AbstractDQNAgent(Agent):
+    """
+    Write me
+    """
+
+    def __init__(self, nb_actions, memory, gamma=.99, batch_size=32, nb_steps_warmup=1000,
+                 train_interval=1, memory_interval=1, target_model_update=10000,
+                 delta_range=None, delta_clip=np.inf, custom_model_objects={}, **kwargs):
+        super(AbstractDQNAgent, self).__init__(**kwargs)
+
+        # Soft vs hard target model updates.
+        if target_model_update < 0:
+            raise ValueError('`target_model_update` must be >= 0.')
+        elif target_model_update >= 1:
+            # Hard update every `target_model_update` steps.
+            target_model_update = int(target_model_update)
+        else:
+            # Soft update with `(1 - target_model_update) * old + target_model_update * new`.
+            target_model_update = float(target_model_update)
+
+        if delta_range is not None:
+            warnings.warn(
+                '`delta_range` is deprecated. Please use `delta_clip` instead, which takes a single scalar. For now '
+                'we\'re falling back to `delta_range[1] = {}`'.format(
+                    delta_range[1]))
+            delta_clip = delta_range[1]
+
+        # Parameters.
+        self.nb_actions = nb_actions
+        self.gamma = gamma
+        self.batch_size = batch_size
+        self.nb_steps_warmup = nb_steps_warmup
+        self.train_interval = train_interval
+        self.memory_interval = memory_interval
+        self.target_model_update = target_model_update
+        self.delta_clip = delta_clip
+        self.custom_model_objects = custom_model_objects
+
+        # Related objects.
+        self.memory = memory
+
+        # State.
+        self.compiled = False
+
+    def process_state_batch(self, batch):
+        batch = np.array(batch)
+        if self.processor is None:
+            return batch
+
